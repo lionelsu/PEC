21aa93fa2f38a145bd1392d3d4f47b8ea037aada
lionelsu <lionel_suicune@hotmail.com>
2022-12-07 21:31:47 -0400

    feat: form complete validation

--- assets/css/validation/input.js
+++ assets/css/validation/input.js
@@ -1,41 +1,61 @@
-const errorTypes = [
-    'valueMissing',
-    'customError',
-    'badInput'
-]
+(() => {
+    const errorTypes = [
+        'valueMissing',
+        'customError',
+        'badInput'
+    ]
 
-const errorMessages = {
-    coe: {
-        valueMissing: 'Preencha o coeficiente por favor.',
-        customError: 'Preencha o coeficiente por favor.',
-        badInput: 'Utilize os operadores corretamente.'
-    }
-}
-
-function showErrorMessage(inputType, input) {
-    let message = ''
-
-    errorTypes.forEach(error => {
-        if(input.validity[error]) {
-            message = errorMessages[inputType][error]
+    const errorMessages = {
+        coea: {
+            valueMissing: 'Preencha o coeficiente por favor.'
+        },
+        coeb: {
+            valueMissing: 'Preencha o coeficiente por favor.'
+        },
+        coec: {
+            valueMissing: 'Preencha o coeficiente por favor.'
         }
-    })
-
-    return message
-}
-
-function validate(input) {
-    const inputType = input.dataset.type
-    
-    if(!input.validity.valid) {
-        input.classList.add('form__input--error')
-        document.querySelector('.input__error').innerHTML = 'Deu certoooooooooo'
     }
-}
 
-const inputs = document.querySelectorAll('input')
-inputs.forEach(input => {
-    input.addEventListener('blur', (event) => {
-        validate(event.target)
+    function showErrorMessage(inputType, input) {
+        let message = ''
+
+        errorTypes.forEach(error => {
+            if(input.validity[error]) {
+                message = errorMessages[inputType][error]
+            }
+        })
+
+        return message
+    }
+
+
+    function validate(input) {
+
+        const dontBreakSolution = input.nextElementSibling.matches('.box__result') 
+        ? input.nextElementSibling.classList.remove('input__error') 
+        : input.nextElementSibling.classList.add('input__error')
+        
+        const inputType = input.dataset.type
+        
+
+        if(!input.validity.valid) {
+            input.classList.add('form__input--error')
+            input.parentElement.querySelector('.input__error--invalid').innerHTML = showErrorMessage(inputType, input)
+            input.nextElementSibling.classList.remove('input__error')
+            
+        } 
+        if(input.validity.valid) {
+            input.classList.remove('form__input--error')
+            input.parentElement.querySelector('.input__error--invalid').innerHTML = ''
+            dontBreakSolution
+        }
+    }
+
+    const inputs = document.querySelectorAll('input')
+    inputs.forEach(input => {
+        input.addEventListener('blur', (event) => {
+            validate(event.target)
+        })
     })
-})
+})()
